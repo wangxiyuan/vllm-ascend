e907bc53024a0238ec8a0dc398be9495635be183
wangxiyuan <wangxiyuan1007@gmail.com>
2026-09-01 18:34:43 +0800

    [Test][CI] update skip_tests
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

--- tests/e2e/pull_request/four_card/context_parallel/test_accuracy_v2.py
+++ tests/e2e/pull_request/four_card/context_parallel/test_accuracy_v2.py
@@ -25,8 +25,6 @@ from dataclasses import dataclass
 from typing import Any, cast
 from unittest.mock import patch
 
-import pytest
-
 from tests.e2e.conftest import DPVllmRunner, VllmRunner, wait_until_npu_memory_free
 
 MAX_NUM_SEQS = 4
@@ -141,7 +139,6 @@ FULL_FEATURE_MODEL_CASES = AccuracyCase(
 )
 
 
-@pytest.mark.skip("The use case is unstable and temporarily taken offline.")
 @patch.dict(
     os.environ,
     {
